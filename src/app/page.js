8397da4a8cafaec255c9d8699a58e5ac15535bfa
burkedyl <burkedyl@msu.edu>
2023-10-14 13:54:10 -0500

    changed basic structure and components

--- src/app/page.js
+++ src/app/page.js
@@ -1,66 +1,137 @@
 "use client";
 import Image from 'next/image'
-import {useEffect, useState} from "react";
-import { useNavigate } from "react-router-dom"
-import React, { Component } from 'react';
+import {useState} from "react";
+import React from 'react';
 
-class ArrowToggleButton extends Component {
-  constructor() {
-    super();
-    this.state = {
-      direction: '\u2227',
-      textVisible: false,
-    };
-  }
+function Section(props) {
+  const [showDetails, setShowDetails] = useState(false);
 
-  handleClick = () => {
-    this.setState((prevState) => ({
-      direction: prevState.direction === '\u2227' ? '\u2228' : '\u2227',
-      textVisible: !prevState.textVisible,
-    }));
-  };
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.direction}
-      </button>
-    );
-  }
+  return (
+    <div className=" m-10 p-10">
+      <h2 className="text-[1.5rem] col-span-2">{props.title}
+        <button
+          className="cursor-pointer text-[#fff] p-5 border-none"
+          onClick={() => setShowDetails(!showDetails)}
+        >
+          {showDetails ? '\u2228' : '\u2227'}
+        </button>
+      </h2>
+      
+      {showDetails && (
+        <p className=" mt-10">
+          {props.text}
+        </p>
+      )}
+    </div>
+  );
 }
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center space-y-12">
+    <main className="flex flex-col max-h-screen">
+      <Image
+        className='rounded-full place-self-center'
+        src="/team-photo-noBg.png"
+        alt="My Face"
+        width={180}
+        height={180}
+        priority 
+      />
+      <div className='text-center text-3xl'>Dylan Burke</div>
+        
+        <Section title="About Me" text={
+          <div>
+            Hello, I'm Dylan, a passionate computer science graduate from Michigan State University. Throughout my academic journey, I've developed a strong 
+            foundation in computer science, cultivating my skills in software development, algorithms, and problem-solving. I thrive on tackling complex challenges 
+            and turning them into innovative solutions.Collaboration is at the heart of my work ethic. I've had the privilege of collaborating with diverse teams, 
+            which has not only broadened my perspective but also enhanced my ability to communicate and adapt in dynamic environments.
+            As an eager learner, I'm committed to staying updated with the latest trends and technologies in the ever-evolving field of computer science. I believe 
+            that continuous learning is the key to staying at the forefront of innovation.
+          </div>}
+        />
+        
+        <Section title="Course Work" text={
+          <div className='w-1/2'>
+            <ul className='ml-10'>
+
+              <li>Collaborative Design (Capstone)</li>
+              <ul className='pl-5 list-disc list-inside'>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+              </ul>
+              
+              <li>Advanced Web Development and Database Management</li>
+              <ul className='pl-5 list-disc list-inside'>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+              </ul>
+
+              <li>Web Application Architecture and Development</li>
+              <ul className='pl-5 list-disc list-inside'>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+              </ul>
+
+              <li>Web Design and Development</li>
+              <ul className='pl-5 list-disc list-inside'>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+              </ul>
+
+              <li>Introduction to Machine Learning</li>
+              <ul className='pl-5 list-disc list-inside'>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+              </ul>
+
+              <li>Database Systems</li>
+              <ul className='pl-5 list-disc list-inside'>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+              </ul>
+
+            </ul>
+          </div>}
+        />
+
+        <Section title="Work Experience" text={
+          <div>
+            <h1>Web/Business Development & Sales Intern</h1>
+            <h2>Future Media Consulting</h2>
+            <h3>May 2022 - August 2022</h3>
+
+            <ul className='list-disc list-inside ml-5'>
+              <li>
+                Assisted in user requirements gathering sessions with key business stakeholder; resulting in a roadmap for delivery 
+              </li>
+              <li>
+                Developed, tested and launched new business web application utilizing WordPress
+              </li>
+              <li>
+                Conducted local search engine optimization improvements 
+              </li>
+              <li>
+                Assisted in conducting inital customer sales inquires
+              </li>
+            </ul>
+          </div>}
+        
+        
+        />
       
-      <div className='h-1/4 flex-col justify-center'>
-        <Image
-            className='rounded-full'
-            src="/team-photo-noBg.png"
-            alt="My Face"
-            width={180}
-            height={180}
-            priority 
-          />
-          <div className='text-center text-3xl'>Dylan Burke</div>
-      </div>
+      <Section title="Skills" text={
+        <div>
 
+        </div>}
+      />
 
-      <div className="flex flex-col text-left text-3xl space-y-5">
-
-        <div> About Me <ArrowToggleButton elementId='about'/>
-          <div id='about' style={{display:'none'}}>
-            Test
-          </div>
-        </div>
-        <div> Education <ArrowToggleButton/></div>
-        <div> Work Experience <ArrowToggleButton/></div>
-        <div> Leadership Experience <ArrowToggleButton/></div>
-        <div> Skills <ArrowToggleButton/></div>
-
-      </div>
-      
     </main>
   )
 }
